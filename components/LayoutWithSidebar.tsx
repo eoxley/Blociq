fc5d6796a4fdb846b503c9d0a350005615af02e6
Ellie Oxley <eleanor.oxley@example.com>
2025-07-11 16:44:00 +0100

    Add LayoutWithSidebar and apply to Inbox

---
+++ components/LayoutWithSidebar.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Sidebar from './sidebar'
+import { ReactNode } from 'react'
+
+export default function LayoutWithSidebar({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 bg-[#f6fafa] p-10">{children}</main>
+    </div>
+  )
+}
