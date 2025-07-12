cd56eda38152d010eb659c247eaac53fa1ee3649
Ellie Oxley <eleanor.oxley@example.com>
2025-07-12 11:40:55 +0100

    Redirect / to /login and update route tree

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,2 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { Home } from 'lucide-react';
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
-
-export default async function HomePage() {
-  const supabase = createServerComponentClient({ cookies });
-  const { data: { session } } = await supabase.auth.getSession();
-
-  if (!session?.user) {
-    redirect('/login'); // not logged in
-  }
-
-  redirect('/');Home// logged in
-}
+import { redirect } from 'next/navigation'
+  redirect('/login')
