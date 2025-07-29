60b1ffb69050808bcd61f6e751e7f4f7488cfa5a
Ellie Oxley <eleanor.oxley@example.com>
2025-07-29 10:18:17 +0200

    🚀 Push all changes + deploy BlocIQ

--- app/compliance/page.tsx
+++
@@ -1,9 +0,0 @@
-import ComplianceChecklist from '@/components/ComplianceChecklist';
-
-export default function CompliancePage() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <ComplianceChecklist />
-    </div>
-  );
-} 
