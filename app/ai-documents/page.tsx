bd2a641681f9f6682e62c769c66f49de05b5f1c3
Ellie Oxley <ellie@Mac.Home>
2025-08-21 15:44:40 +0100

    deploy: pushed via GitHub

--- app/ai-documents/page.tsx
+++
@@ -1,9 +0,0 @@
-import AIDocumentCreator from '@/components/AIDocumentCreator';
-
-export default function AIDocumentsPage() {
-  return (
-    <div className="container mx-auto py-8">
-      <AIDocumentCreator />
-    </div>
-  );
-} 
