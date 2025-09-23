adddf891818059140a34ac134704f90f8d8df40c
Ellie Oxley <eleanor.oxley@blociq.co.uk>
2025-09-23 14:45:15 +0100

    fix: resolve JSX syntax error in building documents page

--- app/(dashboard)/buildings/[id]/documents/page.tsx
+++ app/(dashboard)/buildings/[id]/documents/page.tsx
@@ -125,7 +125,7 @@ export default async function BuildingDocumentsPage({ params }: BuildingDocument
             </a>
           </div>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
